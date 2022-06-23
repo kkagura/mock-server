188bbeaf0e8ed195d45114ee564aea49179176c2
wangj <wangjianweb@sijibao.com>
2022-06-23 13:44:36 +0800

    注释

--- dist/index.js
+++ dist/index.js
@@ -10763,9 +10763,14 @@ function request(config) {
     // 如果传入了mock字段，则向mock服务发起请求
     // 注：项目中实际使用时，需要添加环境判断
     // if (config.mock) {
+    //   // 修改method为post
+    //   config.method = 'post;
+    //   // 把mock参数作为请求体
     //   config.data = config.mock;
+    //   // 修改配置里的url为mock服务地址
     //   config.url = "/mock";
     // }
+    // 没有mock服务的时候
     if (config.mock) {
         return new Promise((resolve) => {
             setTimeout(() => {
