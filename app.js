3618f585e50f5225ac455d382f9543c22c43e178
wangj <wangjianweb@sijibao.com>
2022-06-23 10:46:47 +0800

    fix: 前端mock的方案

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ const Mock = require("mockjs");
 
 app.use(static("dist"));
 app.use(body());
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With"
+  );
+  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  if (ctx.method == "OPTIONS") {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
 const router = new Router();
 router.post("/mock", async (ctx) => {
   const data = Mock.mock(ctx.request.body);
   ctx.body = {
-    code: 0,
+    code: 200,
     data,
     message: "",
   };
--- frontend/request.ts
+++ frontend/request.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
+import mock from "mockjs";
 
 const instance = axios.create();
 
@@ -11,14 +12,22 @@ class RequestError extends Error {
 export function request<T = any>(
   config: AxiosRequestConfig & { mock?: any }
 ): Promise<T> {
-  //  如果传入了mock字段，则向mock服务发起请求
-  if (config.mock /** 注：项目中实际使用时，需要添加环境判断 */) {
-    config.data = config.mock;
-    config.url = "/mock";
+  // 如果传入了mock字段，则向mock服务发起请求
+  // 注：项目中实际使用时，需要添加环境判断
+  // if (config.mock) {
+  //   config.data = config.mock;
+  //   config.url = "/mock";
+  // }
+  if (config.mock) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(mock.mock(config.mock));
+      }, 200 * Math.random());
+    });
   }
   return instance.request<T>(config).then((response: any) => {
     const { code, data, message } = response.data;
-    if (code == "0") {
+    if (code == "200") {
       return data;
     }
     return Promise.reject(new RequestError(message, code));
